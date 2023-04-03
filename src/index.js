639becd075788e65ff767482bc1831c4f911a306
Alok-as07 <aloksahooas07@gmail.com>
2023-04-03 13:04:33 +0530

    env variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app=express();
 const path = require("path");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 000;
 const hbs = require('hbs');
 const weatherService = require("../public/weatherService");
 const getFormattedWeatherData = weatherService.getFormattedWeatherData;  
