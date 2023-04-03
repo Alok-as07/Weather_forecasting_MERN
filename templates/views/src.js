4f177fb835ae75c7b49c4a81a9db5416bd3c4604
Alok-as07 <aloksahooas07@gmail.com>
2023-04-03 12:12:17 +0530

    adding files of weather app mern

---
+++ templates/views/src.js
@@ -0,0 +1,11 @@
+// const city = document.getElementById('city');
+// const submit = document.getElementById('submit');
+// submit.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//      return city;
+//     })
+// console.log(city);
+// const demo =()=>{
+//     return city;
+// }
+// module.exports = demo
